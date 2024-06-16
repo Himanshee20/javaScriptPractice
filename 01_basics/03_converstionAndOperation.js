826d5e7d599092e19e962efdf19b3ec0b54626d8
Himashee pawar <70097171+Himanshee20@users.noreply.github.com>
2024-06-16 17:09:30 +0000

    conversion

---
+++ 01_basics/03_converstionAndOperation.js
@@ -0,0 +1,41 @@
+let age = '24abc';
+
+// console.log(typeof age);
+// console.log(typeof(age));
+
+let valueInNumber = Number(age);
+// console.log(typeof valueInNumber); //number 
+// console.log(valueInNumber);   //NAN 
+
+// "33" => 33
+// "33abc" => NAN
+// true => 1, fasle=> 0
+
+
+let isLoggedIn = 1;
+
+let booleanCov = Boolean(isLoggedIn);
+//console.log(booleanCov); 
+
+// 1=> true ; 0=> false
+// "" => false ; "hp" => true
+
+// ****************************** Operations ********************************************
+
+let  number = 3
+let negValue = -number;
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%3);
+// console.log(2/3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "2");
+
